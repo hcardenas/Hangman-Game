f654053b1cb415441ff639962c22520b0309497f
Helmut Cardenas <hcardenas@Helmuts-MBP.attlocal.net>
2017-06-21 01:22:17 -0400

    finished sounds and comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var current_word ;
 var hidden_word;
 var current_word_arr;  
 var already_guessed = [];
-var number_of_guess = 20;
+var number_of_guess = 0;
 
 set_up(1);
 
@@ -15,33 +15,41 @@ document.onkeyup = function(event) {
 	if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8) {			  
 
 		game(char);
+		// main game function		
 
 
-		
-
+		// checks if the player lost 
 		if (number_of_guess === 0) {
-			alert("Game over, Please try again!");
+			play_sound("you_lose.wav", ("Game over, Please try again!"));
 			set_up(0);
 		}
 
+		// simple print screen function
 		print_screen();
 	    
 	}
 };
 
-
+// -------------------------------
+// main game function it checks if the player won and sets up the game
+// -------------------------------
 function game(char) {
-	// if player already used or guessed do not count down
+	// if player already used or guessed  a letter go in else do nothing
 	if (already_guessed.indexOf(char) === -1) {
+		// wer inside because player used a new letter to push it in the array
 		already_guessed.push(char);
-		
+
+		// sets a flag to check if the letter was used, if 
+		// it was used then do not count down the chances	
 		var flag = 0;
 
 
-
+		// sets up 2 arrays in order to check for indidual letters
 		var hidden_string_arr = hidden_string.split("");
 		var current_word_array = current_word.split("");
 
+
+		// checks if letter is in the current word and updates hidden string
 		for (var i = 0; i < hidden_string.length; i++) {
 			if (char === current_word_array[i]) {
 				hidden_string_arr[i] = char;
@@ -49,6 +57,7 @@ function game(char) {
 			}
 		}
 
+		// creates new hidden string with letters guessed
 		var new_string = "";
 		for (var i = 0; i < hidden_string_arr.length; i++) {
 			new_string += hidden_string_arr[i];
@@ -57,39 +66,51 @@ function game(char) {
 		hidden_string = new_string;
 
 
-		
-
-
+		// if player has no more letters to guess win
 		if (hidden_string === current_word ) {
+			play_sound("win2.mp3", ("Congratulations... the word was.  ' " + current_word + "'.") );
 			win_num++;
-			alert("Congratulations... the word was' " + current_word + "'.");
 			set_up(0);
-
 		}
-		
+
+		// if flag is still 0 then the letter wasnt in the current letter count down
 		if (flag === 0) {
 			number_of_guess--;
 		}
 	}
-
-	
 }
 
+// -------------------------------
+// dounc function that also dysplais the win/lose message
+// -------------------------------
+function play_sound (sound, message) {
+	var audio = new Audio('assets/sounds/' + sound);
+	audio.play();
+	confirm(message);
+}
+
+// -------------------------------
+// sets up variables if screen is new iwth flag1 
+// else simple picks a new word removes old from array and re prints screen
+// -------------------------------
 function set_up (flag) {
 
 	already_guessed = [];
-	number_of_guess = 20;
 
 	if (flag === 1) {
 		word_array_setUp();
+		number_of_guess = Math.floor(current_word.length  / 2);
+		print_screen();
 	}
 	else {
-		current_word = new_element(); 	
+		current_word = new_element();
+		number_of_guess = Math.floor(current_word.length  / 2);
 	}
-
-	print_screen();
 }
 
+// -------------------------------
+// creates string to dysplay on screen the letters already used
+// -------------------------------
 function guess_string() {
 	var string = "";
 	for (var i = 0; i < already_guessed.length; i++) {
@@ -99,6 +120,10 @@ function guess_string() {
 	return string;
 }
 
+// -------------------------------
+// sets up the array to be used for the game, this function is only
+// called when the page is first loaded 
+// -------------------------------
 function word_array_setUp () {
 	var arr = [
 		"Results",
@@ -158,13 +183,22 @@ function word_array_setUp () {
 	current_word_arr = arr;
 	current_word = new_element();
 }
+
+// -------------------------------
+// sets up a new word from the array and creates the hidden string 
+// using that string
+// -------------------------------
 function new_element() {
 	var word = current_word_arr[Math.floor(Math.random()*current_word_arr.length)];
 	remove_elem (current_word_arr, word);
 	hidden_string_func(word);
+
 	return word;
 }
 
+// -------------------------------
+// removes teh current word from the workign array
+// -------------------------------
 function remove_elem (array, element) {
 	var index = array.indexOf(current_word);
 	if (index > -1) {
@@ -172,6 +206,9 @@ function remove_elem (array, element) {
 	}
 }
 
+// -------------------------------
+// creates the hidden string used on screen from the selected current string
+// -------------------------------
 function hidden_string_func(str) {
 	var string = "";
 	for (var i = 0; i < str.length - 1; i++) {
@@ -183,6 +220,9 @@ function hidden_string_func(str) {
 	hidden_string = string;
 }
 
+// -------------------------------
+// print to screen function
+// -------------------------------
 function print_screen() {
 		console.log("current word: " + current_word + "\ncurrent hidden word: " + hidden_string );
 		document.getElementById("win_num").innerHTML = win_num ;
